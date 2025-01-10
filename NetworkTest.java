df5b8053765dbf8c18a58e4781d41f6b059a3254
Edenbitran <eden.bitran@post.runi.ac.il>
2025-01-10 15:32:25 +0200

    user+ netwotk wo followeeCount and mostpopularUser

--- NetworkTest.java
+++ NetworkTest.java
@@ -2,19 +2,23 @@ public class NetworkTest {
     public static void main(String[] args) {
         System.out.println("Testing the Network class...\n");
 
-        System.out.println("...To get started, creating a dummy network for testing the toString and getUser methods...\n");
+        System.out.println(
+                "...To get started, creating a dummy network for testing the toString and getUser methods...\n");
         Network dummyNet = new Network(100, true);
         System.out.println(dummyNet);
 
         System.out.println("Bar is a user in this network: " + ((dummyNet.getUser("Bar") != null) ? true : false));
         System.out.println("Gil is a user in this network: " + ((dummyNet.getUser("Gil") != null) ? true : false));
-     
+
+        System.out.println(dummyNet);
         System.out.println("\n...Now starts the serious testing...");
-        
+
         // Creates a network with a maximum capacity of 1000 users
         Network net = new Network(1000);
+        System.out.println(net);
+        System.out.println(net.getUserCount());
 
-        // Adds Users and follows relationships
+        // // Adds Users and follows relationships
         System.out.println("\n...Adding Users...");
         net.addUser("Alex");
         net.addUser("Orly");
@@ -25,6 +29,8 @@ public class NetworkTest {
         net.addUser("Or");
         net.addUser("Uri");
         net.addUser("Maya");
+
+        System.out.println("~~~~~~~~~`");
 
         System.out.println("\n...Adding follows relationships...");
         net.addFollowee("Alex", "Keren");
@@ -41,11 +47,17 @@ public class NetworkTest {
         System.out.println(net);
 
         System.out.println("\n...Suggesting to Alex which user to follow...");
-        System.out.println("Alex: based on our social network analysis, we recommemd to follow " + net.recommendWhoToFollow("Alex"));
-        
+        System.out.println("Alex: based on our social network analysis, we recommemd to follow "
+                + net.recommendWhoToFollow("Alex"));
+
+        System.out.println("***");
+        System.out.println(net.recommendWhoToFollow("Alex"));
+
+        System.out.println("****");
+
         System.out.println("\n...Finding the most popular user...");
         System.out.println("The most popular user is: " + net.mostPopularUser());
 
-        System.out.println("\nAll Network class tests completed.");
+        // System.out.println("\nAll Network class tests completed.");
     }
 }
--- UserTest.java
+++ UserTest.java
@@ -3,13 +3,17 @@ public class UserTest {
     public static void main(String[] args) {
         System.out.println("...Testing the User class...\n");
 
-        System.out.println("...To get started, creating a dummy user for testing the toString and follows methods...\n");
-        User dummy = new User("Dummy", true);
-        System.out.println(dummy);
-        System.out.println("Dummy follows Bar: " + dummy.follows("Bar"));
-        System.out.println("Dummy follows Gil: " + dummy.follows("Gil"));
+        // System.out
+        // .println("...To get started, creating a dummy user for testing the toString
+        // and follows methods...\n");
+        // User dummy = new User("Dummy", true);
+        // System.out.println(dummy);
+        // System.out.println("Dummy follows Bar: " + dummy.follows("Bar"));
+        // System.out.println("Dummy follows Gil: " + dummy.follows("Gil"));
+        // System.out.println("Dummy add to follows Gil:" + dummy.addFollowee("Gil"));
+        // System.out.println(dummy); // prints all the followee that dummy follow
 
-        System.out.println("\n...Now starts the serious testing..\n");
+        // System.out.println("\n...Now starts the serious testing..\n");
 
         System.out.println("...Creating a user named Alex...");
         User alex = new User("Alex");
@@ -19,10 +23,11 @@ public class UserTest {
         alex.addFollowee("Ben");
         alex.addFollowee("Neta");
         alex.addFollowee("Dana");
+
         System.out.println(alex);
 
         System.out.println("\n...Trying to add Ben again...");
-        alex.addFollowee("Ben");    
+        alex.addFollowee("Ben");
 
         System.out.println("\n...Trying to add Or, Zohar, Dror, Josh, Idan, Uri, Maya, and Dan to Alex...");
         alex.addFollowee("Or");
@@ -32,7 +37,7 @@ public class UserTest {
         alex.addFollowee("Idan");
         alex.addFollowee("Uri");
         alex.addFollowee("Maya");
-        alex.addFollowee("Dan"); // Exceeds the array limit           
+        alex.addFollowee("Dan"); // Exceeds the array limit
         System.out.println(alex);
 
         System.out.println("\n...Removing Neta...");
@@ -46,34 +51,36 @@ public class UserTest {
         System.out.println("\n...Creating a user named Orly...");
         User orly = new User("Orly");
         System.out.println(orly);
-        
+
         System.out.println("\n...Adding Boaz, Maya, Talia, Alex, and Dror to the follows list of Orly...");
         orly.addFollowee("Boaz");
         orly.addFollowee("Maya");
         orly.addFollowee("Talia");
+        orly.addFollowee("Alex");
         orly.addFollowee("Dror");
         System.out.println(orly);
 
         // Tests the countMutual method
         System.out.println("\n...Counting how many users are followed by both Alex and Orly...");
-        System.out.println("The number of users that both Alex and Orly follow: " + alex.countMutual(orly));
-        System.out.println("The number of users that both Orly and Alex follow: " + orly.countMutual(alex));
+        System.out.println("The number of users that both Alex and Orly follow: " +
+                alex.countMutual(orly));
+        System.out.println("The number of users that both Orly and Alex follow: " +
+                orly.countMutual(alex));
 
         // Tests the isFriend method
         System.out.println("\n...Checking if Alex and Orly are friends...");
         System.out.println("Alex is a friend of Orly: " + alex.isFriendOf(orly));
         System.out.println("Orly is a friend of Alex: " + orly.isFriendOf(alex));
-        
-        // Making Alex and Orly friends
+
+        // // Making Alex and Orly friends
         System.out.println("\n...Adding Orly to the follows list of Alex, and Alex to the follows list of Orly...");
         alex.addFollowee("Orly");
         orly.addFollowee("Alex");
 
-        // Tests the isFriend method
         System.out.println("\n...Checking if Alex and Orly are friends...");
         System.out.println("Alex is a friend of Orly: " + alex.isFriendOf(orly));
         System.out.println("Orly is a friend of Alex: " + orly.isFriendOf(alex));
-        
-        System.out.println("\nAll User class tests completed.");
+
+        // System.out.println("\nAll User class tests completed.");
     }
 }
