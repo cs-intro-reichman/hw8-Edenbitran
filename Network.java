237edb293dcaac9cf23a50214f9c681ec6512238
Edenbitran <eden.bitran@post.runi.ac.il>
2025-01-10 19:26:41 +0200

    all

--- Network.java
+++ Network.java
@@ -86,11 +86,8 @@ public class Network {
      */
     public boolean addFollowee(String name1, String name2) {
         //// Replace the following statement with your code
-        if (getUser(name1) != null && getUser(name2) != null && !name1.equals(name2)) {
+        if (name1 != null && name2 != null && !name1.equals(name2)) {
             return getUser(name1).addFollowee(name2);
-        }
-        if (name1 == null) {
-            return false;
         }
         return false;
     }
